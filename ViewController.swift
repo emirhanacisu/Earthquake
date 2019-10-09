ad24868a6773ff1eb9b2402e66f51abe72f45043
emirhan acısu <emirhan.acisu@gmail.com>
2019-10-09 20:00:19 +0300

    coordinate

--- ViewController.swift
+++ ViewController.swift
@@ -13,25 +13,36 @@ import  MapKit
 
 
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
- var locationManager = CLLocationManager()
+ 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    var ref : DocumentReference? = nil
+    let db = Firestore.firestore()
     override func viewDidLoad() {
         super.viewDidLoad()
        
-       locationManager.delegate = self
+        mapView.delegate = self
+        locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
-      
+        
         
         
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let span = MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03)
         let region = MKCoordinateRegion(center: location, span: span)
-        print(location.latitude)
-        print(location.longitude)
+        mapView.setRegion(region,animated : true)
+        print(locations[0].coordinate.latitude)
+        print(locations[0].coordinate.longitude)
+         ref = db.collection("locations").addDocument(data: ["latitude" : locations[0].coordinate.latitude, "longitude" : locations[0].coordinate.longitude])
+      
+        
+        
     }
 
 }
